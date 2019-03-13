7615697930b22f8f21c318555a07f32978a6c738
Junyoung McArdle <juny.mcardle@gmail.com>
2019-03-13 18:48:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,9 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
-  if(string.upcase)
-    'I LOVE YOU TOO PUMPKIN!'
+  if(string == "I LOVE YOU GRANDMA")
+    "I LOVE YOU TOO PUMPKIN!"
+  elsif(string == string.upcase)
+    "NO, NOT SINCE 1938!"
   else
     'HUH? SPEAK UP, SONNY!'
   end
